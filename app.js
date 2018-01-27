57bd599190f2ccb11303f5d810c45a7699b606e4
RiCK BROWN <ricky.brown.00@gmail.com>
2018-01-27 17:08:59 +0000

    added keyboard functionality

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ let io = require('socket.io');
 let five = require('johnny-five');
 let temporal = require('temporal');
 let colors = require('colors');
+let keypress = require('keypress');
 
 // Create the board instance
 let board = new five.Board({ port: "COM3" });
@@ -35,40 +36,78 @@ board.on("ready", function(){
     //     }
     // });
 
-    temporal.queue([
-        {
-            delay: 5000,
-            task: function(){
-                console.log("MOVING FORWARDS".green);
-                left_wheel.cw();
-                right_wheel.ccw();
-            }
-        }, {
-            delay: 3000,
-            task: function(){
-                console.log("STOPPING".red);
-                left_wheel.stop();
-                right_wheel.stop();
-            }
-        }, {
-            delay: 3000,
-            task: function(){
-                console.log("MOVING BACKWARDS".green);
-                left_wheel.ccw();
-                right_wheel.cw();
-            }
-        }, {
-            delay: 3000,
-            task: function(){
-                console.log("STOPPING".red);
-                left_wheel.stop();
-                right_wheel.stop();
-            }
-        }, {
-            delay: 1500,
-            task: function(){
-                console.log("TEST COMPLETE. QUITTING APP!".blue);
-            }
+    // // Temporal.queue will allow automated movement for 2nd testing
+    // temporal.queue([
+    //     {
+    //         delay: 5000,
+    //         task: function(){
+    //             console.log("MOVING FORWARDS".green);
+    //             left_wheel.cw();
+    //             right_wheel.ccw();
+    //         }
+    //     }, {
+    //         delay: 3000,
+    //         task: function(){
+    //             console.log("STOPPING".red);
+    //             left_wheel.stop();
+    //             right_wheel.stop();
+    //         }
+    //     }, {
+    //         delay: 3000,
+    //         task: function(){
+    //             console.log("MOVING BACKWARDS".green);
+    //             left_wheel.ccw();
+    //             right_wheel.cw();
+    //         }
+    //     }, {
+    //         delay: 3000,
+    //         task: function(){
+    //             console.log("STOPPING".red);
+    //             left_wheel.stop();
+    //             right_wheel.stop();
+    //         }
+    //     }, {
+    //         delay: 1500,
+    //         task: function(){
+    //             console.log("TEST COMPLETE. QUITTING APP!".blue);
+    //         }
+    //     }
+    // ]);
+
+    // Keyboard movement
+    // Make PROCESS.STDIN begin emiitting KEYPRESS events
+    keypress(process.stdin);
+
+    // Listen for the KEYPRESS event
+    process.stdin.on('keypress', function(ch, key){
+        console.log('"KEYPRESS" RECIEVED'.green, key);
+        if(key && key.ctrl && key.name == 'd'){
+            console.log('PAUSING'.green);
+            process.stdin.pause();
+        } else if(key && key.name == 'up'){
+            console.log('FORWARD'.green);
+            left_wheel.cw();
+            right_wheel.ccw();
+        } else if(key && key.name == "space"){
+            console.log('STOP'.green);
+            left_wheel.stop();
+            right_wheel.stop();
+        } else if(key && key.name == 'left'){
+            console.log('LEFT'.green);
+            left_wheel.ccw();
+            right_wheel.ccw();
+        } else if(key && key.name == 'right'){
+            console.log('RIGHT'.green);
+            left_wheel.cw();
+            right_wheel.cw();
+        } else if(key && key.name == 'down'){
+            console.log('BACKWARD'.green);
+            left_wheel.cw();
+            right_wheel.ccw();
         }
-    ]);
+    });
+
+    process.stdin.setRawMode(true);
+    process.stdin.resume();
+
 }); // End of board.on function
